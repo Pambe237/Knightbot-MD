d8fc18dcc312e5049b3cb934f5f520065daf2bf8
Pambe237 <pambefrancois44@gmail.com>
2025-04-10 14:44:56 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Pambe',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Pambe bot",
+  botOwner: 'Professor', // Your na
+  ownerNumber: '237681028390', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
